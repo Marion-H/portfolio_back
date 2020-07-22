8fc38861a4b6f7f3ad29cc343529e776f4a666fb
Marion-H <doudou6500@gmail.com>
2020-07-22 11:44:52 +0200

    create a route post contact

--- route/contact.route.js
+++ route/contact.route.js
@@ -1,14 +1,27 @@
-const express = require('express')
-const contact = express.Router()
-const Contact = require('../model/Contact')
+const express = require("express");
+const contact = express.Router();
+const Contact = require("../model/Contact");
 
-contact.get('/', async(req,res)=>{
-    try {
-        const contact = Contact.findAll()
-        res.status(200).json(contact)
-    } catch (error) {
-        res.status(400).json(err)
-    }
-})
+contact.get("/", async (req, res) => {
+  try {
+    const contacts = await Contact.findAll();
+    res.status(200).json(contacts);
+  } catch (error) {
+    res.status(400).json(err);
+  }
+});
 
-module.exports = contact
+contact.post("/", async (req, res) => {
+  const { name, link, logo } = req.body;
+  try {
+    const contact = await Contact.create({
+      name,
+      link,
+      logo
+    });
+    res.status(201).json(contact)
+  } catch (error) {
+      res.status(422).json(err)
+  }
+});
+module.exports = contact;
